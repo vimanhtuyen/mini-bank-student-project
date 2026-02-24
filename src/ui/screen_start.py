ed679a6dc0fbb81c3d153941a72a31062b5025ff
namphong123456 <peterphong812@gmail.com>
2026-02-24 20:18:14 +0700

    message

---
+++ src/ui/screen_start.py
@@ -0,0 +1,41 @@
+from tkinter import ttk
+
+class StartFrame(ttk.Frame):
+    def __init__(self, parent, app):
+        super().__init__(parent)
+        self.app = app
+
+    title_label = ttk.Label(self, text="MINI BANK", style="Title.TLabel")
+        title_label.pack(pady=14)
+
+        description_label = ttk.Label(
+            self,
+            text="test 1", # co the thay doi dc
+            justify="test 2",# co the thay doi dc
+        )
+        description_label.pack(pady=8)
+
+        button_frame = ttk.Frame(self)
+        button_frame.pack(pady=20)
+
+        ttk.Button(button_frame, text="Tạo tài khoản", command=self.go_register, width=28).grid(row=0, column=0, pady=8)
+        ttk.Button(button_frame, text="Đăng nhập", command=self.go_login, width=28).grid(row=1, column=0, pady=8)
+        ttk.Button(button_frame, text="Thoát", command=self.app.on_window_close, width=28).grid(row=2, column=0, pady=8)
+
+        tips_frame = ttk.LabelFrame(self, text="test 3")# co the thay doi dc
+        tips_frame.pack(pady=18, padx=40, fill="x")
+
+        tips_text = (
+    
+        )
+        ttk.Label(tips_frame, text=tips_text, justify="left").pack(anchor="w", padx=10, pady=10)
+
+
+
+
+
+    def go_register(self) -> None:
+        self.app.show_frame("RegisterFrame")
+
+    def go_login(self) -> None:
+        self.app.show_frame("LoginFrame")
